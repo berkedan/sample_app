288489034d1496ec9a39c2b7998d112d6982e0d7
Dan Berke <danb@mobilibuy.com>
2014-04-13 17:41:23 +0300

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,22 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
+
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
