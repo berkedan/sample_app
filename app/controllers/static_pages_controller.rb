708b8b0bf93fb53b51c24b94db19fca90828b7d3
Dan Berke <danb@mobilibuy.com>
2014-04-16 10:39:56 +0300

    init for chapter six

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
     logger.info("info")
+   #  test only
+   # user = User.new(name: 'dan', email: "mhartl@example.com")
+   # user.save
   end
 
   #test
